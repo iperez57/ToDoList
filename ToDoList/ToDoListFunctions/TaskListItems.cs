7e58ed700e8e8179a67babf6922c592d687de6c5
Isaac Perez <Isaac.Perez.1@outlook.com>
2025-02-04 22:34:53 -0800

    added a new class called tasklistitems that holds all properties that a task should have in the todo list.

---
+++ ToDoList/ToDoListFunctions/TaskListItems.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListFunctions
+{
+    public class TaskListItems
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string Priority { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateModified { get; set; } = DateTime.Now;
+
+    }
+}
